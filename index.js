b1a777e18c51f682c42f3217d749622fa818d81b
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-05-07 02:07:07 +0600

    show os name on boot added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const CLI = require('./src/cli.js')
 const Config = require('./src/config.js')
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+console.log(
+  chalk.green(
+    figlet.textSync('MonkeOS', { horizontalLayout: 'full' })
+  )
+);
+
 
 const config = new Config()
 
