0169b843033f467b885e10649ecadfdeceb02569
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-05-08 00:16:34 +0600

    two new commands added clear and inspire

---
+++ src/commands/inspire.js
@@ -0,0 +1,36 @@
+
+const fetch = require('node-fetch');
+class inspire
+{
+	constructor() {
+       
+    }
+
+	run(next)
+	{ 
+        // fetch("")
+        // .then(res => res.json())
+        // .then(data =>{
+        //     console.log(data.content);
+        // })
+
+        fetch("https://api.quotable.io/random")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Network issue found!");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data.content);
+          console.log(" -" + data.author);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+
+		next()
+	}
+}
+
+module.exports = inspire
